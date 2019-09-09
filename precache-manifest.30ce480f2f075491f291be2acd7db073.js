7db552265b98a254dd18a78fbc80e70e7986220f
arunavaDe <arunava.de@gmail.com>
2019-09-09 16:12:11 -0400

    Updates

---
+++ precache-manifest.30ce480f2f075491f291be2acd7db073.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3c6523c6b2639913059c93b1d705b9",
+    "url": "/projectFiveNew/index.html"
+  },
+  {
+    "revision": "555fa487187cf010cfe6",
+    "url": "/projectFiveNew/static/css/main.093a54a1.chunk.css"
+  },
+  {
+    "revision": "ee1401b4f9f7a41cc881",
+    "url": "/projectFiveNew/static/js/2.217cbff7.chunk.js"
+  },
+  {
+    "revision": "555fa487187cf010cfe6",
+    "url": "/projectFiveNew/static/js/main.faba41ad.chunk.js"
+  },
+  {
+    "revision": "ea440ee99c75d737af95",
+    "url": "/projectFiveNew/static/js/runtime~main.6956db1b.js"
+  }
+]);
